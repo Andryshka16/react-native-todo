ff3c44bead7277c87348c5f2e412c9f77cb4dbf8
Andrey Lapchik <a.lapchik7@gmail.com>
2022-10-27 11:10:35 +0300

    27 10

--- components/Todos.js
+++ components/Todos.js
@@ -1,21 +1,29 @@
 
 import React from "react";
-import {Text, View, StyleSheet, Button, FlatList} from "react-native";
+import {Text, View, StyleSheet, Button, FlatList, TouchableOpacity} from "react-native";
 
 function Todo({title, date, removeTodo}){
 
-    return <View style={styles.view}>
-        <Text style={styles.text}>
-            {title}
-        </Text>
-        <Text style={styles.text}>
-            {date.toLocaleDateString()}
-        </Text>
-        <Text style={styles.text}>
-            {date.toLocaleTimeString()}
-        </Text>
-        <Button title={"DONE"} onPress={removeTodo}/>
-    </View>
+    return (
+        <View style={styles.view}>
+            <TouchableOpacity
+                style={styles.touchAbleOpacity}
+                activeOpacity={0.6}
+                onLongPress={()=>console.log(title, date)}
+            >
+                <Text style={styles.text}>
+                    {title}
+                </Text>
+                <Text style={styles.text}>
+                    {date.toLocaleDateString()}
+                </Text>
+                <Text style={styles.text}>
+                    {date.toLocaleTimeString()}
+                </Text>
+            </TouchableOpacity>
+            <Button title={"DONE"} onPress={removeTodo}/>
+        </View>
+    )
 }
 
 export default function Todos({todos, setTodos}){
@@ -39,15 +47,21 @@ const styles = StyleSheet.create({
     view: {
         backgroundColor: "#747474",
         flexDirection: "row",
-        justifyContent: "space-between",
         margin: 10,
         marginTop: 0,
-        borderRadius: 5
+        borderRadius: 5,
+        width: 390
     },
     text: {
         fontSize: 20,
-        marginHorizontal: 20,
+        marginHorizontal: 15,
         marginVertical: 5,
+        maxWidth: 100,
         color: "white"
+    },
+    touchAbleOpacity: {
+        flexDirection: "row",
+        width: 340,
+        justifyContent: "space-between",
     }
 })
